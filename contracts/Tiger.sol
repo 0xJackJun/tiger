e3fbbbc15c930ecd23c7964345ba9d798c4dfe16
quanrong <quanrong@hashquark.io>
2022-02-16 16:11:52 +0800

    format

--- contracts/Tiger.sol
+++ contracts/Tiger.sol
@@ -7,62 +7,100 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
 contract Tiger is ERC721URIStorage,Ownable{
-
+    //tokenId counter
     uint256 private counter;
+    
+    //check if address is in whitelist or not
     mapping(address => bool) whitelist;
+
+    //check if address has minted or not
     mapping(address => bool) minted;
+
+    //check if address has interacted or not
     mapping (uint256 => mapping(address => bool)) interacted;
+
+    //mapping address to blessings
     mapping (address => Status) blessings;
+
+    //blessing status
     enum Status{
         Init,Zore,Twelve,TwentyFour,ThirtySix,FoutyEight,Sixty,SeventyTwo,EighttyFour,NinetySix,NotTiger
     }
 
+    //mint begin time
     uint256 mintBegin = 1648252800;
+
+    //whitelist end time
     uint256 whitelistEnd = 1647957600;
+
+    //whitelist begin time
     uint256 whitelistBegin = 1647950400;
 
     constructor(string memory name, string memory symbol) 
     ERC721(name,symbol)
     {}
 
+    /**
+    * @dev Guarantees msg.sender is in whitelist
+    */
     modifier onlyWhitelist(){
         require(whitelist[_msgSender()] == true,"caller is not in whitelist");
         _;
     }
 
+    /**
+    *@dev Guarantees msg.sender hasn't minted before
+     */
     modifier notMinted(){
         require(minted[_msgSender()] == false, "already minted");
         _;
     }
 
+    /**
+    *@dev Guarantees msg.sender with specific tokenId hasn't interacted before
+    *@param tokenId tokenId that msg.sender owned
+     */
     modifier notInteracted(uint tokenId){
         require(interacted[tokenId][_msgSender()] == false);
         _;
     }
     
-    function addWhitelist(address to) external onlyOwner{
+    /**
+    *@dev add address to whitelist map
+     */
+    function addWhitelist(address to)
+    external onlyOwner {
         require(to!=address(0),"invalid address");
         whitelist[to] = true;
     }
 
-    function mint() public notMinted{
+    /**
+    * @dev mint tiger NFT and init status
+     */
+    function mint()
+    public notMinted {
         if(block.timestamp >= whitelistBegin || block.timestamp <= whitelistEnd){
             require(whitelist[_msgSender()] == true,"caller is not in whitelist");
             _safeMint(_msgSender(),counter);
             blessings[_msgSender()] = Status.Init;
             minted[_msgSender()] = true;
             counter += 1;
-        } else if(block.timestamp >= mintBegin){
-        
-        _safeMint(_msgSender(),counter);
-        blessings[_msgSender()] = Status.Init;
-        minted[_msgSender()] = true;
-        counter += 1;
+        } else if (block.timestamp >= mintBegin) {
+            _safeMint(_msgSender(),counter);
+            blessings[_msgSender()] = Status.Init;
+            minted[_msgSender()] = true;
+            counter += 1;
         } else {
             revert();
         }
     }
 
+    /**
+    *@dev calculate the blessings of user
+    *@param year year of birth
+    *@param tokenId specific tokenId user owned
+    *@return status generated for user
+     */
     function interact(uint256 year, uint256 tokenId) public notInteracted(tokenId) returns(Status){
         require(_isApprovedOrOwner(_msgSender(), tokenId), "not owner nor approved");
         interacted[tokenId][_msgSender()] == true;
