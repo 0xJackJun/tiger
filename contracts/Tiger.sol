195b189c9dc0c4ea3c031e62b84afea439ddfefe
quanrong <quanrong@hashquark.io>
2022-02-17 18:55:59 +0800

    test

--- contracts/Tiger.sol
+++ contracts/Tiger.sol
@@ -10,6 +10,9 @@ contract Tiger is ERC721URIStorage,Ownable{
     //tokenId counter
     uint256 private counter;
     
+    //init price
+    uint256 private price = 10 wei;
+
     //check if address is in whitelist or not
     mapping(address => bool) whitelist;
 
@@ -19,24 +22,29 @@ contract Tiger is ERC721URIStorage,Ownable{
     //check if address has interacted or not
     mapping (uint256 => mapping(address => bool)) interacted;
 
-    //mapping address to blessings
-    mapping (address => Status) blessings;
+    //mapping tokenId to blessings
+    mapping (uint256 => string) blessings;
+
+    //mapping tokenId to status index
+    mapping(uint256 => uint) index;
+
+    //image url
+    string[] private image = [
+        'https://arweave.net/UDtoZ6MIfkHS_ann_4Vd0eGjsTIMDtG62KPyY2WWyVU',
+        'https://arweave.net/L-sJQCHkxN7Lp1jnnCI6FE8QzhrJ0fGKIZHvHk0-1lY',
+        'https://arweave.net/TsBjAI85WGYxGxn2DW4qe8q_S5sfZoVx1e10LOAx6JA',
+        'https://arweave.net/sChUEpEHXRHjYWxNhSQaHsVDB5Y3aDYfm2byzuDoRJI',
+        'https://arweave.net/E54w6pNdsnP3sURt7xJ53-sA5n1OYHo9Cov-Tb22320',
+        'https://arweave.net/JgM0LpkTkhDuUkWKzcI9U1HTA2KeOOyxPrNOgOp0W1g',
+        'https://arweave.net/5-IyU7xPGQtI9smyB7_2qkOdXmpAQXu_0bOCRoUGA5Q',
+        'https://arweave.net/CN722GXJbVTKeEzunsD0-lrTso2Zp-z_m4O6DPuj5s0',
+        'https://arweave.net/_PH2KEAOJyLQ2qHHaPKA_gItxC9yLF2uLzT4MpxFtoA',
+        'https://arweave.net/ew-vjO2gXu134J2USd5eGfeV9FbNcn5TcejpaKw4Y2A',
+        'https://arweave.net/_I0gUOAn0Ym3h8JXl8M_ZyNASzrNjLPGvZU29x-CGBY'
+    ];
 
     //blessing status
-    enum Status{
-        Null,
-        Init,
-        Zore,
-        Twelve,
-        TwentyFour,
-        ThirtySix,
-        FoutyEight,
-        Sixty,
-        SeventyTwo,
-        EighttyFour,
-        NinetySix,
-        NotTiger
-    }
+    string[] private status = ["Init","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","NotTiger"];
 
     //tiger year begin
     int[] yearBegin = [int(-2142633600),-1765065600,-1384819200,-1007251200,-627091200,-249436800,128131200,508291200,885945600,1266105600,1643673600,2023920000,2401488000,2779056000,3159302400,3536870400,3917030400];
@@ -53,8 +61,8 @@ contract Tiger is ERC721URIStorage,Ownable{
     //whitelist begin time
     uint256 whitelistBegin = 1647950400;
 
-    constructor(string memory name, string memory symbol) 
-    ERC721(name,symbol)
+    constructor(string memory _name, string memory _symbol) 
+    ERC721(_name,_symbol)
     {}
 
     /**
@@ -93,16 +101,19 @@ contract Tiger is ERC721URIStorage,Ownable{
     /**
     * @dev mint tiger NFT and initialize status
      */
-    function mint() public notMinted {
+    function mint() public payable notMinted {
+        require(msg.value == price);
         if(block.timestamp >= whitelistBegin || block.timestamp <= whitelistEnd){
             require(whitelist[_msgSender()] == true,"caller is not in whitelist");
             _safeMint(_msgSender(),counter);
-            blessings[_msgSender()] = Status.Init;
+            blessings[counter] = status[0];
+            index[counter] = 0;
             minted[_msgSender()] = true;
             counter += 1;
         } else if (block.timestamp >= mintBegin) {
             _safeMint(_msgSender(),counter);
-            blessings[_msgSender()] = Status.Init;
+            blessings[counter] = status[0];
+            index[counter] = 0;
             minted[_msgSender()] = true;
             counter += 1;
         } else {
@@ -116,48 +127,113 @@ contract Tiger is ERC721URIStorage,Ownable{
     *@param tokenId specific tokenId user owned
     *@return status generated for user
      */
-    function interact(uint256 year, uint256 tokenId) public notInteracted(tokenId) returns(Status) {
+    function interact(uint256 year, uint256 tokenId) public notInteracted(tokenId) returns(string memory) {
         require(_isApprovedOrOwner(_msgSender(), tokenId), "not owner nor approved");
         interacted[tokenId][_msgSender()] == true;
         (uint cycle,uint remainder) = ((2022-year) / 12,(2022 - year) % 12);
         if (remainder != 0){
-            blessings[_msgSender()] = Status.NotTiger;
+            blessings[tokenId] = status[10];
+            index[tokenId] = 10;
         }else if(cycle == 0){
-            blessings[_msgSender()] = Status.Zore;
+            blessings[tokenId] = status[1];
+            index[tokenId] = 1;
         }else if(cycle == 1){
-            blessings[_msgSender()] = Status.Twelve;
+            blessings[tokenId] = status[2];
+            index[tokenId] = 2;
         }else if(cycle == 2){
-            blessings[_msgSender()] = Status.TwentyFour;
+            blessings[tokenId] = status[3];
+            index[tokenId] = 3;
         }else if(cycle == 3){
-            blessings[_msgSender()] = Status.ThirtySix;
+            blessings[tokenId] = status[4];
+            index[tokenId] = 4;
         }else if(cycle == 4){
-            blessings[_msgSender()] = Status.FoutyEight;
+            blessings[tokenId] = status[5];
+            index[tokenId] = 5;
         }else if(cycle == 5){
-            blessings[_msgSender()] = Status.Sixty;
+            blessings[tokenId] = status[6];
+            index[tokenId] = 6;
         }else if(cycle == 6){
-            blessings[_msgSender()] = Status.SeventyTwo;
+            blessings[tokenId] = status[7];
+            index[tokenId] = 7;
         }else if(cycle == 7){
-            blessings[_msgSender()] = Status.EighttyFour;
+            blessings[tokenId] = status[8];
+            index[tokenId] = 8;
         }else if(cycle == 8){
-            blessings[_msgSender()] = Status.NinetySix;
+            blessings[tokenId] = status[9];
+            index[tokenId] = 9;
         }else{
-            blessings[_msgSender()] = Status.Zore;
+            blessings[tokenId] = status[1];
+            index[tokenId] = 1;
         }
-        return blessings[_msgSender()];
+        return blessings[tokenId];
     }
 
-    function getTokenURI(uint256 tokenId) public view returns(Status) {
+    function calJson(uint256 tokenId,uint i) private view returns (string memory){
+        string memory strTokenId = _toString(tokenId);
+        string memory json = Base64.encode(
+            bytes(
+            string(
+            abi.encodePacked(
+            '{"id":"',
+            strTokenId,
+            '","name":"tiger#',
+            strTokenId,
+            '","image":',
+            image[i],
+            ',"description":"tiger year nft","attributes":[{"trait_type":"status","value":',
+            status[i],
+            ',}]}'
+        )
+        )
+        )
+        );
+        string memory output = string(abi.encodePacked("data:application/json;base64,",json));
+        return output;
+    } 
+
+    function tokenURI(uint256 tokenId) public override view returns(string memory) {
         require(ownerOf(tokenId) != address(0), "token not exist");
         for(uint i = 0; i < 16; i++){
             if(int(block.timestamp) >= yearBegin[i] || int(block.timestamp) <= yearEnd[i]){
-                return blessings[_msgSender()];
+                return calJson(tokenId,index[tokenId]);
             } else {
                 if(interacted[tokenId][_msgSender()]){
-                    return Status.NinetySix;
+                    return calJson(tokenId,9);
                 }else {
-                    return Status.Init;
+                    return calJson(tokenId,0);
                 }
             }
         }
+        return "";
+    }
+
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override {
+        interacted[tokenId][from] = false;
+    }
+
+    function _toString(uint256 value) internal pure returns (string memory) {
+        // Inspired by OraclizeAPI's implementation - MIT license
+        // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
