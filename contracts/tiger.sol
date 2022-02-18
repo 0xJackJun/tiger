b4837316a4a9c1fdf7903c33a572500ae6a9c8ac
quanrong <quanrong@hashquark.io>
2022-02-18 16:46:06 +0800

    edit ERC721Enum

--- contracts/tiger.sol
+++ contracts/tiger.sol
@@ -1,13 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 
 
-contract Tiger is ERC721URIStorage,Ownable{
+contract Tiger is ERC721Enumerable,Ownable{
     //tokenId counter
     uint256 private counter;
     
